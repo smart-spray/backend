f92359bd027ecca40174c94872f2bef8632377fb
MarceloMax50 <maxmelo93@gmail.com>
2022-02-19 15:32:55 -0300

    feature/nodeToCloud: Cria endpoint para enviar dados para a placa

--- src/modules/pulverization/pulverization.service.ts
+++ src/modules/pulverization/pulverization.service.ts
@@ -1,9 +1,9 @@
+import SendCloudToDeviceMessage from "../../iotHub/SendCloudToDeviceMessage";
 import { IRepository } from "../../repositories/repository";
 import { PulverizationEntity } from "./pulverization.entity";
 
 export default class PulverizationService {
   constructor(private repository: IRepository<PulverizationEntity>) {}
-
   public async retrievePulverization(
     pulverizationId: string
   ): Promise<PulverizationEntity> {
@@ -24,5 +24,16 @@ export default class PulverizationService {
 
   public async listAll(): Promise<PulverizationEntity[]> {
     return await this.repository.findAll();
+  }
+
+  public async sendMessageToCloud(message: string) {
+    const validMessages = ["Limpeza", "Pulverização"];
+    if (validMessages.includes(message)) {
+      const sendCloudMessage = new SendCloudToDeviceMessage();
+      await sendCloudMessage.sendMessage(message);
+    }
+    throw new Error(
+      "Invalid message, it must be one of 'Limpeza' or 'Pulverização'"
+    );
   }
 }
