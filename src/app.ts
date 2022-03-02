c4b6f2868d1b32714cfb1b750ac3eb3218329a0f
Leandro <leandrofernandesvieira@gmail.com>
2022-03-02 17:42:56 -0300

    fix: add cors to socket

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,11 @@
+import cors from "cors";
 import express from "express";
 
 import routes from "./routes";
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, X-Custom-Header");
-
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-
-  next();
-});
-
+app.use(cors);
 app.use(express.json());
 app.use(routes);
 
