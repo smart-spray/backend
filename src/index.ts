7a4d2df1867fa6db07ed7c120fb7f3593f3b92bc
leandrovi <leandrofernandesvieira@gmail.com>
2022-04-01 11:30:05 -0300

    fix: use correct port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import server from "./app";
 import { connectMongo } from "./infrastructure/mongo/connection";
 
-const port = process.env.PORT || 3334;
+const port = process.env.PORT || 3333;
 
 server.listen(port, async () => {
   await connectMongo();
