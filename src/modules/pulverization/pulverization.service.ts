fa8b84008bfdd0f9826b995163f32a79c150e3e1
leandrovi <leandrofernandesvieira@gmail.com>
2022-05-23 16:39:54 -0300

    refactor: add back climate api

--- src/modules/pulverization/pulverization.service.ts
+++ src/modules/pulverization/pulverization.service.ts
@@ -46,7 +46,7 @@ export class PulverizationService {
     const { deviceId, city, state } = params;
 
     const telemetryService = new TelemetryService();
-    // const climatempoService = new ClimatempoService();
+    const climatempoService = new ClimatempoService();
 
     const [phAndTurbidityTelemetry] =
       await telemetryService.listAllPhAndTurbidity();
@@ -58,18 +58,7 @@ export class PulverizationService {
     const { isClean, ph, createdAt } = phAndTurbidityTelemetry;
     const { status: nozzleStatus } = flowRateTelemetry;
 
-    // const data = await climatempoService.getWeather({ city, state });
-    const data = {
-      data: {
-        temperature: 32,
-        wind_direction: "ESE",
-        wind_velocity: 16.2,
-        humidity: 95.1,
-        condition: "Chuva",
-        pressure: 937.8,
-        sensation: 19,
-      },
-    };
+    const data = await climatempoService.getWeather({ city, state });
 
     const {
       temperature,
@@ -144,7 +133,7 @@ export class PulverizationService {
     }
 
     if (humidity < 55) {
-      errors += " baixa umidade do ar, ";
+      errors += "baixa umidade do ar, ";
       haveErrors = true;
     }
 
